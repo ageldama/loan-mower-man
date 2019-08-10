ae72cbdb89d72980442da83d85ac2e9af099f36d
Jong-Hyouk Yun <ageldama@gmail.com>
2019-08-10 15:05:51 +0900

    spring initilized!

---
+++ src/main/java/jhyun/loanmowerman/LoanMowerManApplication.java
@@ -0,0 +1,13 @@
+package jhyun.loanmowerman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMowerManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMowerManApplication.class, args);
+	}
+
+}
