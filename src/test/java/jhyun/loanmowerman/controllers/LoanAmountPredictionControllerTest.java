7326a3ad9fe5b1f6ef5196140a8e3d0993196e67
Jong-Hyouk Yun <ageldama@gmail.com>
2019-08-11 23:05:20 +0900

    --wip-- [skip ci]

--- src/test/java/jhyun/loanmowerman/controllers/LoanAmountPredictionControllerTest.java
+++ src/test/java/jhyun/loanmowerman/controllers/LoanAmountPredictionControllerTest.java
@@ -2,14 +2,13 @@ package jhyun.loanmowerman.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.ReadContext;
 import jhyun.loanmowerman.controllers.aggregations.NoDataException;
 import jhyun.loanmowerman.controllers.predictions.LoanAmountPredictionOfMonthRequest;
 import jhyun.loanmowerman.services.ApiUserService;
 import jhyun.loanmowerman.services.LoanAmountHistoryService;
 import jhyun.loanmowerman.services.LoanAmountPredictionService;
 import jhyun.loanmowerman.services.predictions.AveragePredictor;
+import jhyun.loanmowerman.services.predictions.LoanAmountPrediction;
 import jhyun.loanmowerman.services.predictions.PredictionNotPreparedException;
 import jhyun.loanmowerman.testing_supp.Examples;
 import jhyun.loanmowerman.testing_supp.WebMvcTestBase;
@@ -29,7 +28,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -64,20 +64,36 @@ public class LoanAmountPredictionControllerTest extends WebMvcTestBase {
     }
 
     @Test
-    public void testPredictWithAverage() throws JsonProcessingException, NoDataException, PredictionNotPreparedException {
+    public void testPredictWithAverage() throws IOException, NoDataException, PredictionNotPreparedException {
+        given(averagePredictor.predict(any(), any(), anyString())).willReturn(
+                LoanAmountPrediction.builder()
+                        .amount(123L)
+                        .bank("BANKCODE")
+                        .year(1234)
+                        .month(24)
+                        .build()
+        );
+        //
+        final ObjectMapper objectMapper = new ObjectMapper();
+        //
         final LoanAmountPredictionOfMonthRequest request = LoanAmountPredictionOfMonthRequest.builder()
                 .bank("국민은행")
                 .month(2)
                 .build();
         final HttpHeaders headers = apiUserJwtHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        final ObjectMapper objectMapper = new ObjectMapper();
+
         final HttpEntity request2 = new HttpEntity(objectMapper.writeValueAsString(request), headers);
         final ResponseEntity<String> response = restTemplate.exchange(
                 apiBase() + "/predictions/?strategy=average", HttpMethod.POST,
                 request2, String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        log.info("prediction-result = {}", response.getBody());
+        final LoanAmountPrediction predictionResult = objectMapper.readValue(response.getBody(), LoanAmountPrediction.class);
+        assertThat(predictionResult).isNotNull();
+        assertThat(predictionResult.getAmount()).isEqualTo(123L);
+        assertThat(predictionResult.getBank()).isEqualTo("BANKCODE");
+        assertThat(predictionResult.getYear()).isEqualTo(1234);
+        assertThat(predictionResult.getMonth()).isEqualTo(24);
         //
         verify(averagePredictor, times(1)).predict(
                 eq(2018), eq(2), eq("3")
